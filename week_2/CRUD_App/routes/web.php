a0c29b9ade5c4532c13709c1a8d734821376eb6c
SatrioHubs <satrioramadhani387@gmail.com>
2025-02-18 08:42:32 +0700

    up new

--- week_2/CRUD_App/routes/web.php
+++ week_2/CRUD_App/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ItemController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,5 +16,3 @@ use App\Http\Controllers\ItemController;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('items', ItemController::class);
