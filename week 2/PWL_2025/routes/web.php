81984c852acbb48ab9ea2bd47a33c68c0a498a51
SatrioHubs <satrioramadhani387@gmail.com>
2025-02-27 15:40:11 +0700

    Controller

--- week 2/PWL_2025/routes/web.php
+++ week 2/PWL_2025/routes/web.php
@@ -21,17 +21,17 @@ Route::get('/', function () {
     return 'Selamat Datang';
 });
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+// Route::get('/hello', function () {
+//     return 'Hello World';
+// });
 
 Route::get('/world', function () {
     return 'World';
 });
 
-Route::get('/about', function () {
-    return 'NIM : 23241720163<br>Nama : Satrio Ahmad Ramadhani ';
-});
+// Route::get('/about', function () {
+//     return 'NIM : 23241720163<br>Nama : Satrio Ahmad Ramadhani ';
+// });
 
 Route::get('/user/{name}', function ($name) {
     return 'Nama saya '.$name;
