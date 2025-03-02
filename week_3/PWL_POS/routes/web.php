f95e077fed8660af576f0a03710824b8d4f25850
SatrioHubs <satrioramadhani387@gmail.com>
2025-03-03 06:57:23 +0700

    praktikum 5

--- week_3/PWL_POS/routes/web.php
+++ week_3/PWL_POS/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\KategoriController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 /*
@@ -18,3 +19,4 @@ Route::get('/', function () {
 });
 
 Route::get('/level', [LevelController::class, 'index']);
+Route::get('/kategori', [KategoriController::class, 'index']);
